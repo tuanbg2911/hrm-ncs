36cefb064df407722ec89438c5bdeebbd79265ea
tuanbg2911 <hocontapdanh@gmail.com>
2023-03-07 11:43:50 +0700

    addnewcode2

--- ncs_employee/models/contact.py
+++ ncs_employee/models/contact.py
@@ -1,9 +1,18 @@
 from odoo import fields,models
 
 class Contacts(models.Model):
-    _name = 'contact'
+    _inherit = 'hr.contract'
+
 
     x_kpi_income = fields.Monetary('Lương KPI')
     x_overtime_income = fields.Monetary('Lương tăng ca')
     x_diligence = fields.Monetary('Chuyên cần')
+    salary_per_month = fields.Monetary('Tiền công/tiền lương')
+    sup_home = fields.Monetary('Hỗ trợ nhà ở')
+    sup_gas = fields.Monetary('Hỗ trợ xăng xe')
+    sup_food = fields.Monetary('Phụ cấp tiền ăn')
+    sup_medical = fields.Monetary('Phụ cấp y tế')
+    sup_other = fields.Monetary('Phụ cấp khác')
+    sup_phone = fields.Monetary('Phụ cấp điện thoại')
+    sup_da = fields.Monetary('DA')
     currency_id = fields.Many2one('res.currency', string='Tiền tệ', tracking=True,)
--- ncs_employee/models/hr_employee_inherit.py
+++ ncs_employee/models/hr_employee_inherit.py
@@ -4,7 +4,9 @@ class HrEmployeeInherit(models.Model):
     _inherit = 'hr.employee'
 
     number_people_deducted = fields.Integer('Số lượng người được giảm trừ', readonly=True, compute='_number_people_deducted')
-    x_group_age = fields.Many2one('hr.employee.age', readonly=True, compute='_compute_group_age')
+    x_group_age = fields.Many2one('hr.employee.age')
+    group_age = fields.Integer('Nhóm tuổi', readonly=True)
+                               # , compute='_compute_group_age')
     age = fields.Many2one('hr.employee.age')
     x_dependent = fields.One2many('hr.employee.dependent', 'x_employee_id')
 
@@ -17,15 +19,15 @@ class HrEmployeeInherit(models.Model):
             rec.number_people_deducted = len(lst)
 
 
-    @api.depends('age.x_min_age')
-    def _compute_group_age(self):
-        mylist = []
-        for rec in self:
-            years = datetime.datetime.now().year - rec.x_dependent.x_dob.year
-            if years > rec.age.x_min_age:
-                for i in range(0, years):
-                    mylist.append(i)
-                    rec.x_group_age = min(mylist, key=lambda x: abs(x - years))
+    # @api.depends('age.x_min_age')
+    # def _compute_group_age(self):
+    #     mylist = []
+    #     for rec in self:
+    #         years = datetime.datetime.now().year - rec.x_dependent.x_dob.year
+    #         if years > rec.age.x_min_age:
+    #             for i in range(0, years):
+    #                 mylist.append(i)
+                    # rec.group_age = min(mylist, key=lambda x: abs(x - years))
 
 # Lấy ra 1 bản ghi thỏa mãn điều kiện: years > x_min_age  và  x_min_age gần years nhất.
 
